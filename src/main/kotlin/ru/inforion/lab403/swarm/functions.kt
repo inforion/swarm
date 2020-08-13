75d55e8d7664ae4a6f52629c019f9574bef1acc2
Simankov Artem <a.simankov@inforion.ru>
2020-08-13 13:40:20 +0300

    функция swarm перенесена в OK

--- src/main/kotlin/ru/inforion/lab403/swarm/functions.kt
+++ src/main/kotlin/ru/inforion/lab403/swarm/functions.kt
@@ -16,12 +16,6 @@ fun mpiSwarm(vararg args: String, compress: Boolean = false, code: (Swarm) -> Un
     Swarm(MPI(*args, compress = compress), code)
 }
 
-fun swarm(name: String, compress: Boolean, vararg args: Any, code: (Swarm) -> Unit) = when(name) {
-    "THREAD" -> threadsSwarm(args[0] as Int, compress, code)
-    "MPI" -> mpiSwarm(*args as Array<out String>, compress = compress, code = code)
-    else -> throw IllegalArgumentException("Unknown realm '$name' for Swarm!")
-}
-
 fun <T> Collection<T>.separate(count: Int): Collection<Collection<T>> {
     require(count > 0) { "Collection size must be > 0" }
     val chunkSize = size / count + 1
